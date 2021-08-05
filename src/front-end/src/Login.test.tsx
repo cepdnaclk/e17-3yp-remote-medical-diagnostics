d386994ae736cd88d0475e6b1de6f813d3e6c465
Udith <uaudith@gmail.com>
2021-08-05 16:19:21 +0530

    ✅ fix testcases closes cepdnaclk#9

--- src/front-end/src/Login.test.tsx
+++ src/front-end/src/Login.test.tsx
@@ -1,9 +1,17 @@
-import React from "react";
 import { render, screen } from "@testing-library/react";
 import Login from "./Login";
+import { Provider } from "react-redux";
+import Store from "./Store";
+import { MemoryRouter, Route } from "react-router-dom";
 
 test("renders sign in component", () => {
-  render(<Login />);
+  render(
+    <Provider store={Store}>
+      <MemoryRouter initialEntries={["/login"]} initialIndex={0}>
+        <Route path="/login" component={Login} />
+      </MemoryRouter>
+    </Provider>
+  );
   const linkElement = screen.getByText(/Sign in to MedGenie/i);
   expect(linkElement).toBeInTheDocument();
 });
