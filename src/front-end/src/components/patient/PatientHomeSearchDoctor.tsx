d6bc0a78f8ae327d598e65ab631c2a1b88705082
pasindumarasinghe <e17207@eng.pdn.ac.lk>
2021-08-26 12:47:10 +0530

    🚧Implement Search Doctor box in the patient-home page

--- src/front-end/src/components/patient/PatientHomeSearchDoctor.tsx
+++ src/front-end/src/components/patient/PatientHomeSearchDoctor.tsx
@@ -1,35 +1,59 @@
 import React from 'react';
 import "../../App.css"
-// import { } from '@fortawesome/free-solid-svg-icons'
+import{ReactComponent as SearchIcon} from "../../icons/search.svg"
+import{ReactComponent as Calendar} from "../../icons/calendar.svg"
+import{ReactComponent as Globe} from "../../icons/globe.svg"
+import{ReactComponent as Specialization} from "../../icons/specialization.svg"
+import{ReactComponent as Person} from "../../icons/person.svg"
+import { isCallSignatureDeclaration } from 'typescript';
 
 const PatientHomeSearchDoctor = () => {
     return (
         <div className='find-a-doctor'> Find a Doctor
-            <input
-                type="text"
-                placeholder="Name"
-                className="find-a-doctor-input"
-            />
+            <form>
+                <label className="find-a-doctor-input-field">
+                &nbsp;&nbsp;<Person />&nbsp;&nbsp;
+                <input
+                    type="text"
+                    placeholder="Name"
+                    className="find-a-doctor-input"
+                />
+                </label>
 
-            <input
-                type="text"
-                placeholder="Speciality"
-                className="find-a-doctor-input"
-            />
-            <input
-                type="text"
-                placeholder="Language"
-                className="find-a-doctor-input"
-            />
-            <input
-                type="text"
-                placeholder="Date and Time"
-                className="find-a-doctor-input"
-            />
+                <label className="find-a-doctor-input-field">
+                &nbsp;&nbsp;<Specialization />&nbsp;&nbsp;
+                <select className="find-a-doctor-input">
+                    <option value="general-practitioner">General Practitioner</option>
+                    <option value="lime">Lime</option>
+                    <option value="coconut">Coconut</option>
+                    <option value="mango">Mango</option>
+                </select>
+                </label>
 
-            <button className="find-a-doctor-button">
-                Search
-            </button>
+                <label className="find-a-doctor-input-field">
+                &nbsp;&nbsp;<Globe />&nbsp;&nbsp;
+                <select className="find-a-doctor-input">
+                    <option value="sinhala">සිංහල​​</option>
+                    <option value="tamil">தமிழ்</option>
+                    <option value="english">English</option>
+                </select>
+                </label>
+
+                <label className="find-a-doctor-input-field">
+                &nbsp;&nbsp;<Calendar />&nbsp;&nbsp;
+                <input
+                    type="datetime-local"
+                    placeholder="Date and Time"
+                    className="find-a-doctor-input"
+                />
+                </label>
+                
+
+                <button className="find-a-doctor-button">
+                    <SearchIcon />&nbsp; Search 
+                </button>
+
+            </form>
         </div>
     );
 }
