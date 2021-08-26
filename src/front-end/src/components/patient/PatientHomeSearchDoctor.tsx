9147c50606280c9ba72c7c040113f3111f509f94
pasindumarasinghe <e17207@eng.pdn.ac.lk>
2021-08-26 18:42:10 +0530

    🚧Implement Search Doctor box in the patient-home page

--- src/front-end/src/components/patient/PatientHomeSearchDoctor.tsx
+++ src/front-end/src/components/patient/PatientHomeSearchDoctor.tsx
@@ -1,61 +1,69 @@
-import React from 'react';
+import * as React from 'react';
 import "../../App.css"
 import{ReactComponent as SearchIcon} from "../../icons/search.svg"
 import{ReactComponent as Calendar} from "../../icons/calendar.svg"
 import{ReactComponent as Globe} from "../../icons/globe.svg"
 import{ReactComponent as Specialization} from "../../icons/specialization.svg"
 import{ReactComponent as Person} from "../../icons/person.svg"
-import { isCallSignatureDeclaration } from 'typescript';
 
-const PatientHomeSearchDoctor = () => {
-    return (
-        <div className='find-a-doctor'> Find a Doctor
-            <form>
-                <label className="find-a-doctor-input-field">
-                &nbsp;&nbsp;<Person />&nbsp;&nbsp;
-                <input
-                    type="text"
-                    placeholder="Name"
-                    className="find-a-doctor-input"
-                />
-                </label>
 
-                <label className="find-a-doctor-input-field">
-                &nbsp;&nbsp;<Specialization />&nbsp;&nbsp;
-                <select className="find-a-doctor-input">
-                    <option value="general-practitioner">General Practitioner</option>
-                    <option value="lime">Lime</option>
-                    <option value="coconut">Coconut</option>
-                    <option value="mango">Mango</option>
-                </select>
-                </label>
 
-                <label className="find-a-doctor-input-field">
-                &nbsp;&nbsp;<Globe />&nbsp;&nbsp;
-                <select className="find-a-doctor-input">
-                    <option value="sinhala">සිංහල​​</option>
-                    <option value="tamil">தமிழ்</option>
-                    <option value="english">English</option>
-                </select>
-                </label>
+class PatientHomeSearchDoctor extends React.Component {
 
-                <label className="find-a-doctor-input-field">
-                &nbsp;&nbsp;<Calendar />&nbsp;&nbsp;
-                <input
-                    type="datetime-local"
-                    placeholder="Date and Time"
-                    className="find-a-doctor-input"
-                />
-                </label>
-                
+    handleSubmit: React.FormEventHandler<HTMLFormElement> | undefined = async (e) => {
+        alert("Searching for the Doctor......");
+        e.preventDefault();
+      };
+    
+    render(){
+        return (
+            <div className='find-a-doctor'> Find a Doctor
+                <form onSubmit = {this.handleSubmit}>
+                    <label className="find-a-doctor-input-field">
+                    &nbsp;&nbsp;<Person />&nbsp;&nbsp;
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        className="find-a-doctor-input"
+                    />
+                    </label>
 
-                <button className="find-a-doctor-button">
-                    <SearchIcon />&nbsp; Search 
-                </button>
+                    <label className="find-a-doctor-input-field">
+                    &nbsp;&nbsp;<Specialization />&nbsp;&nbsp;
+                    <select className="find-a-doctor-input">
+                        <option value="general-practitioner">General Practitioner</option>
+                        <option value="cardiologist">Cardiologist</option>
+                        <option value="Allergist">Allergist</option>
+                    </select>
+                    </label>
 
-            </form>
-        </div>
-    );
+                    <label className="find-a-doctor-input-field">
+                    &nbsp;&nbsp;<Globe />&nbsp;&nbsp;
+                    <select className="find-a-doctor-input">
+                        <option value="sinhala">සිංහල​​</option>
+                        <option value="tamil">தமிழ்</option>
+                        <option value="english">English</option>
+                    </select>
+                    </label>
+
+                    <label className="find-a-doctor-input-field">
+                    &nbsp;&nbsp;<Calendar />&nbsp;&nbsp;
+                    <input
+                        type="datetime-local"
+                        placeholder="Date and Time"
+                        className="find-a-doctor-input"
+                    />
+                    </label>
+                    
+
+                    <button type="submit" className="find-a-doctor-button">
+                        <SearchIcon />&nbsp; Search 
+                    </button>
+
+                </form>
+            </div>
+        )
+    }
 }
 
 export default PatientHomeSearchDoctor
