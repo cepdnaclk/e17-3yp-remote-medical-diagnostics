3b101735c64786dc9c9c8419dd8714998a9478c4
pasindumarasinghe <e17207@eng.pdn.ac.lk>
2021-09-12 08:57:41 +0530

    🐛 Fix bugs

--- src/front-end/src/components/patient/PatientDoctors.tsx
+++ src/front-end/src/components/patient/PatientDoctors.tsx
@@ -1,40 +1,41 @@
 import React from "react";
 import PatientHomeSearchDoctor from "./PatientHomeSearchDoctor";
-import {ReactComponent as Closebutton} from "../../icons/close-button.svg"
-
-export interface PatientDoctorsProps {}
-
-export interface PatientDoctorsState {}
-
-class PatientDoctors extends React.Component<PatientDoctorsProps,PatientDoctorsState> {
-    state = {
-      findDoctorPopup:false,
-    };
-
-    togglePopup = () :void =>{//Close the PopUp
-      this.setState({findDoctorPopup : !this.state.findDoctorPopup});
-    }
+import { ReactComponent as Closebutton } from "../../icons/close-button.svg"
 
 
+export interface PatientDoctorsProps { }
 
-    render() {
-      return (
-        <>
-          <button onClick={this.togglePopup} type="button" className="btn btn-primary btn-lg" id="btn-1">Find a Doctor</button>
-            
-            {this.state.findDoctorPopup && (
-              <div className="modl">
-              <div onClick={this.togglePopup} className = "overlay"></div>
-              <div className="find-a-doc-popup">
-                <PatientHomeSearchDoctor />
-              </div>
-              <button onClick = {this.togglePopup} className = "close-modl"><Closebutton /></button>
-            </div>
-            )}
-            
-        </>
-      );
-    }
+export interface PatientDoctorsState { }
+
+class PatientDoctors extends React.Component<PatientDoctorsProps, PatientDoctorsState> {
+  state = {
+    findDoctorPopup: false,
+  };
+
+  togglePopup = (): void => {//Close the PopUp
+    this.setState({ findDoctorPopup: !this.state.findDoctorPopup });
   }
+
+
+
+  render() {
+    return (
+      <>
+        <button onClick={this.togglePopup} type="button" className="btn btn-primary btn-lg" id="btn-1">Find a Doctor</button>
+
+        {this.state.findDoctorPopup && (
+          <div className="modl-pdoc">
+            <div onClick={this.togglePopup} className="overlay-pdoc"></div>
+            <div className="find-a-doc-popup">
+              <PatientHomeSearchDoctor />
+              <button onClick={this.togglePopup} className="close-modl-pdoc"><Closebutton /></button>
+            </div>
+          </div>
+        )}
+
+      </>
+    );
+  }
+}
 
 export default PatientDoctors;
