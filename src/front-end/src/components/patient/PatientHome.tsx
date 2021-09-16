894e824695ad1c4abd645f070bf9bb8a6ec03a83
pasindumarasinghe <e17207@eng.pdn.ac.lk>
2021-09-16 19:07:03 +0530

    :construction:Fix Configure-Device modal

--- src/front-end/src/components/patient/PatientHome.tsx
+++ src/front-end/src/components/patient/PatientHome.tsx
@@ -2,9 +2,12 @@ import React from "react";
 import PatientHomeSearchDoctor from "./PatientHomeSearchDoctor";
 import ConfigDeviceOverlay from "./ConfigDeviceOverlay";
 
-export interface PatientHomeProps { }
+export interface PatientHomeProps {
+  modal_status_global: boolean;
+  closeModal: () => void;
+}
 
-export interface PatientHomeState { }
+export interface PatientHomeState {}
 
 class PatientHome extends React.Component<PatientHomeProps, PatientHomeState> {
   state = {
@@ -16,11 +19,12 @@ class PatientHome extends React.Component<PatientHomeProps, PatientHomeState> {
   };
 
   closePopup = (): void => {
+    this.props.closeModal();
     this.setState({ modl: false });
   };
 
   componentDidMount = (): void => {
-    setTimeout(this.openPopup, 700);
+    setTimeout(this.openPopup, 2000);
   };
 
   render() {
@@ -29,6 +33,7 @@ class PatientHome extends React.Component<PatientHomeProps, PatientHomeState> {
         <PatientHomeSearchDoctor />
         <ConfigDeviceOverlay
           st={this.state.modl}
+          st_global={this.props.modal_status_global}
           closePopup={this.closePopup}
         />
       </>
