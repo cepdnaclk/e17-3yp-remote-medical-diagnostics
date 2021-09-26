44daf7b0fb3d5dbdc698b16b8a14bc86f8cb37ae
Pasindu Marasinghe <66524245+pasindumarasinghe@users.noreply.github.com>
2021-09-26 13:00:05 +0530

    ✨Implement Patient's UIs (#48)
    * Implement patient-doctor table with fetched data using a mock API
    
    * Implement patient-doctor table with fetched data using a mock API
    
    * Bug fixes

---
+++ src/front-end/src/components/Signup.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import "../Styles/Signup.css";
+
+class Signup extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="signup-form">
+          <label className="create-account-label">Create Your Account</label>
+          <form>
+            <div className="row">
+              <div className="form-group col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputText4"
+                  placeholder="First Name"
+                />
+              </div>
+              <div className="form-group col-md-6">
+                <input
+                  type="text"
+                  className="form-control"
+                  id="inputText4"
+                  placeholder="Last Name"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="form-group col-md-12">
+                <input
+                  type="email"
+                  className="form-control"
+                  id="inputEmail4"
+                  placeholder="Email Address"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="form-group col-md-12">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="inputpassword4"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="form-group col-md-12">
+                <input
+                  type="password"
+                  className="form-control"
+                  id="inputpassword4"
+                  placeholder="Confirm Password"
+                />
+              </div>
+            </div>
+
+            <button type="submit" id="sign-up-btn" className="btn btn-primary">
+              Create Account
+            </button>
+          </form>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Signup;
--- src/front-end/src/components/patient/PatientDoctors.tsx
+++ src/front-end/src/components/patient/PatientDoctors.tsx
@@ -1,10 +1,44 @@
 import React from "react";
 import PatientHomeSearchDoctor from "./PatientHomeSearchDoctor";
 import { ReactComponent as Closebutton } from "../../icons/close-button.svg";
+import { Card } from "react-bootstrap";
+import axios from "axios";
 
-export interface PatientDoctorsProps { }
+export interface DoctorProps {
+  doctor: {
+    name: string;
+    speciality: string;
+    age: number;
+    email: string;
+    rating: number;
+  };
+}
 
-export interface PatientDoctorsState { }
+const Doctor = (props: DoctorProps) => {
+  //A doctor component to be put in the list
+  return (
+    <tr>
+      <td>photo</td>
+      <td>{props.doctor.name}</td>
+      <td>{props.doctor.speciality}</td>
+      <td>{props.doctor.age}</td>
+      <td>{props.doctor.email}</td>
+      <td>{props.doctor.rating}</td>
+    </tr>
+  );
+};
+
+export interface PatientDoctorsProps {}
+export interface PatientDoctorsState {
+  findDoctorPopup: boolean;
+  doctors: {
+    name: string;
+    speciality: string;
+    age: number;
+    email: string;
+    rating: number;
+  }[];
+}
 
 class PatientDoctors extends React.Component<
   PatientDoctorsProps,
@@ -12,6 +46,7 @@ class PatientDoctors extends React.Component<
 > {
   state = {
     findDoctorPopup: false,
+    doctors: [],
   };
 
   togglePopup = (): void => {
@@ -19,9 +54,88 @@ class PatientDoctors extends React.Component<
     this.setState({ findDoctorPopup: !this.state.findDoctorPopup });
   };
 
+  doctorList = () => {
+    return this.state.doctors.map((doctor) => {
+      return <Doctor doctor={doctor} key={Math.random()} />; // *****Change the Key*******
+    });
+  };
+
+  getDoctors = async () => {
+    try {
+      const doctors = await axios.get(
+        "https://jsonplaceholder.typicode.com/users" //TODO : API
+      );
+
+      //======================================================================
+      let doc_list: any[] = [];
+      doctors.data.forEach((doc: any) => {
+        doc_list.push({
+          name: doc.name,
+          speciality: doc.username,
+          age: doc.id,
+          email: doc.email,
+          rating: doc.id,
+        });
+      });
+      //=======================================================================
+
+      this.setState({ doctors: doc_list.slice(0, 5) }); //<---- fetched data
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  componentDidMount = () => {
+    //fetch doctors from the database
+    this.getDoctors();
+  };
+
   render() {
     return (
       <>
+        <div className="d-flex mb-auto mt-5">
+          <div>
+            {" "}
+            &nbsp;
+            <div>
+              &nbsp;
+              <h2>DOCTORS</h2> &nbsp;
+            </div>
+            <div>
+              <Card className="rounded shadow p-3 mb-5 bg-white rounded">
+                <table className="table" style={{ width: "800px", margin: 0 }}>
+                  <thead>
+                    <tr>
+                      <th key="pic" scope="col">
+                        {" "}
+                      </th>
+                      <th key="doc" scope="col">
+                        Name
+                      </th>
+                      <th key="esp" scope="col">
+                        Speciality
+                      </th>
+                      <th key="age" scope="col">
+                        Age
+                      </th>
+                      <th key="email" scope="col">
+                        Email
+                      </th>
+                      <th key="rating" scope="col">
+                        Rating
+                      </th>
+                      <th key="add" scope="col">
+                        |Btn|
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>{this.doctorList()}</tbody>
+                </table>
+              </Card>
+            </div>
+          </div>
+        </div>
+
         <button
           onClick={this.togglePopup}
           type="button"
@@ -30,7 +144,6 @@ class PatientDoctors extends React.Component<
         >
           Find a Doctor
         </button>
-
         {this.state.findDoctorPopup && (
           <div className="modl-pdoc">
             <div className="overlay-pdoc"></div>
