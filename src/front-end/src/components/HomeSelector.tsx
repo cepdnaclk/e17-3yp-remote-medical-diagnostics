c62fee612fe298030022df62c75ac41c8a8887df
pasindumarasinghe <e17207@eng.pdn.ac.lk>
2021-10-18 13:11:16 +0530

    Bug Fixes

--- src/front-end/src/components/HomeSelector.tsx
+++ src/front-end/src/components/HomeSelector.tsx
@@ -3,24 +3,12 @@ import { useSelector } from "react-redux";
 import { RootState } from "../store/Store";
 import PatientMetaComponent from "./PatientMetaComponent";
 import DoctorMetaComponent from "./DoctorMetaComponent";
-<<<<<<< HEAD
-//import PatientAppointmentsChatRoom from "./patient/PatientChatRoom";
-=======
 import AdminMeta from "./AdminMetaComponent";
->>>>>>> 6638a23dde521fffe7813fa23623d16ecd1afdde
 
 interface HomeSelectorProps {}
 
 const HomeSelector: FunctionComponent<HomeSelectorProps> = () => {
   const type = useSelector((state: RootState) => state.user.type);
-<<<<<<< HEAD
-  return type === "patient" ? (
-    <PatientMetaComponent />
-  ) : (
-    <DoctorMetaComponent />
-    //<PatientAppointmentsChatRoom />
-  );
-=======
   switch (type) {
     case "patient":
       return <PatientMetaComponent />;
@@ -31,7 +19,6 @@ const HomeSelector: FunctionComponent<HomeSelectorProps> = () => {
     default:
       return <PatientMetaComponent />;
   }
->>>>>>> 6638a23dde521fffe7813fa23623d16ecd1afdde
 };
 
 export default HomeSelector;
