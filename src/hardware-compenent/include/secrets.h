87f31c68ad6d4c51e719d4b7d67f445066225e9e
KavinduJayas <59658804+KavinduJayas@users.noreply.github.com>
2022-11-17 11:37:53 +0530

    Change temprature sensor connectivity to MQTT

--- src/hardware-compenent/include/secrets.h
+++ src/hardware-compenent/include/secrets.h
@@ -1,13 +1,20 @@
 #include <pgmspace.h>
 
-#define SECRET
-#define THINGNAME ""
+const char WIFI_SSID[] = "Eng-Student";
+const char WIFI_PASSWORD[] = "3nG5tuDt";
 
-const char WIFI_SSID[] = "<SSID>";
-const char WIFI_PASSWORD[] = "<PASSWORD>";
-const char AWS_IOT_ENDPOINT[] = "<ENDPOINT>.<REGION>.amazonaws.com";
+const char *deviceUUID = "1234";
 
-const char *deviceUUID = "unique_ID";
+// MQTT Broker
+const char *mqtt_broker = "52.184.165.84";
+const char *mqtt_username = "co326a";
+const char *mqtt_password = "safety";
+const int mqtt_port = 1883;
+
+// The MQTT topics that this device should publish/subscribe
+const char *topic_confirm = "medgenie/1234/confirm";
+const char *topic_pair = "medgenie/1234/pair";
+const char *topic_temperature = "medgenie/1234/temperature";
 
 // Amazon Root CA 1
 static const char AWS_CERT_CA[] PROGMEM = R"EOF(
